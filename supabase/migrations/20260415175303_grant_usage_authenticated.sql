63f2ea8d26815cbc505a2a3a24b6cd34001c1e7c
Jason <jtrallii@live.com>
2026-04-15 14:57:47 -0300

    feat(migration): criado grant usage para authenticated

---
+++ supabase/migrations/20260415175303_grant_usage_authenticated.sql
@@ -0,0 +1,8 @@
+
+
+
+-- Tabela usuarios
+GRANT SELECT, INSERT, UPDATE, DELETE ON operon.usuarios TO authenticated;
+
+-- Tabela clientes
+GRANT SELECT, INSERT, UPDATE, DELETE ON operon.clientes TO authenticated;
